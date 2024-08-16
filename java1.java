5cbeeb4ccba9cb531acc6f534782ee01dc59fff4
sk01410 <150681438+sk01410@users.noreply.github.com>
2024-08-16 19:39:53 +0530

    java intro

--- java1.java
+++ java1.java
@@ -3,6 +3,6 @@
  */
 public class java1 {
         public static void main(String[] args) {
-            System.out.println("congratulations"+" to you");
+            System.out.println("congratulations"+" to you"+" for your first java code");
         }
 }
