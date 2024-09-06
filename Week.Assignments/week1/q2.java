73e68959c26acdaa0d3ab7fe1b15e5c4f01068e3
sk01410 <150681438+sk01410@users.noreply.github.com>
2024-09-06 15:51:14 +0530

    multplication

---
+++ Week.Assignments/week1/q2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class q2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = s.nextInt();
+        for(int i=1;i<=10;i++)
+        System.out.println(n+"x"+i+"="+n*i);
+
+    }
+}
